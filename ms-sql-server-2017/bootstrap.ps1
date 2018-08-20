015cb2872430ba5d4580d62f94449f76e647778e
Danny <danny@afahounko.com>
2018-08-20 04:26:37 +0200

    Update bootstrap.ps1

--- ms-sql-server-2017/bootstrap.ps1
+++ ms-sql-server-2017/bootstrap.ps1
@@ -13,7 +13,7 @@ param
 
 #$PSScriptRoot
 
-& ".\Configure-SQL.ps1"
+#& ".\Configure-SQL.ps1"
 
 #$command = concat($PSScriptRoot,"\Configure-SQL.ps1") 
 #Invoke-Expression $command
@@ -23,3 +23,35 @@ param
 # Invoke-Expression $command
 
 
+
+$sourceDir = "C:\SQL_Server_2017_Developer"
+$patchDir = "C:\Patches.SQL.2017"
+$deploylog = "C:\infra\deploylog"
+$installDir = "C:\infra\install"
+
+$networkSharedDir1 = "Z:\SQL\install"
+$networkSharedDir2 = "Z:\SQL\SSMS"
+$networkSharedDir3 = "Z:\SQL\Patches.SQL.2017"
+$networkSharedDir4 = "Z:\SQL\SQL_Server_2017_Developer"
+
+
+#Check and create folders if not exist
+if (!(Test-Path -path "$sourceDir")) #create it if not existing
+{
+  New-Item "$sourceDir" -type directory | out-null 
+}
+
+if (!(Test-Path -path "$patchDir")) #create it if not existing
+{
+  New-Item "$patchDir" -type directory | out-null 
+}
+
+if (!(Test-Path -path "$deploylog")) #create it if not existing
+{
+  New-Item "$deploylog" -type directory | out-null 
+}
+
+if (!(Test-Path -path "$installDir")) #create it if not existing
+{
+  New-Item "$installDir" -type directory | out-null 
+}
